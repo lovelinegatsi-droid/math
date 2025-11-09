765e4cd238ed0eea9ac57fb7515c616e88882ba6
lovelinegatsi <lovelinegatsi@gmail.com>
2025-11-09 18:00:59 +0100

    mise a jour du readme

--- src/geometry/vector.h
+++ src/geometry/vector.h
@@ -11,16 +11,16 @@ struct Vector2f {
     float y;
 };
 
-Vector2f MakeV2f(float x, float y);
-Vector2f MakeV2f(const Point2f& a, const Point2f& b);
-Vector2f Add(const Vector2f& a, const Vector2f& b);
-Vector2f Sub(const Vector2f& a, const Vector2f& b);
-Vector2f Scale(const Vector2f& v, float scalar);
-float Dot(const Vector2f& a, const Vector2f& b);
-float Length(const Vector2f& v);
-Vector2f Normalize(const Vector2f& v);
-Vector2f Lerp(const Vector2f& a, const Vector2f& b, float t);
-float Determinant(const Vector2f& a, const Vector2f& b);
+Vector2f MakeV2f(float x, float y); // pour declarer un vecteur de coordonnees reels
+Vector2f MakeV2f(const Point2f& a, const Point2f& b); // pour delimiter le vecteur qui part du point a pour b
+Vector2f Add(const Vector2f& a, const Vector2f& b); // pour additonner deux vecteurs
+Vector2f Sub(const Vector2f& a, const Vector2f& b);// pour les soustraires
+Vector2f Scale(const Vector2f& v, float scalar); //pour faire l'homothetie d'un vecteur (l'agrandir ou le reduire)
+float Dot(const Vector2f& a, const Vector2f& b);// pour faire le produit scalaire des vecteurs
+float Length(const Vector2f& v); // pour calculer la norme du vecteur
+Vector2f Normalize(const Vector2f& v); // normaliser un vecteur
+Vector2f Lerp(const Vector2f& a, const Vector2f& b, float t);// permet de faire l'interpolation lineaire entre deux vecteurs
+float Determinant(const Vector2f& a, const Vector2f& b);// calculer le determinant de deux vecteurs
 
-std::string ToString(const Vector2f& v);
+std::string ToString(const Vector2f& v); //afficher le resultat
 #endif//VECTOR_H
